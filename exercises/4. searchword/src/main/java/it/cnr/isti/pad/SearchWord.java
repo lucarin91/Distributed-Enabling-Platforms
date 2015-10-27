9f57313f5fb20dc39436f806e598d41049033bea
Nicola Tonellotto <nicola.tonellotto@gmail.com>
2015-10-27 10:56:11 +0100

    exercise 4 fixed by fconte90. Thx

--- exercises/4. searchword/src/main/java/it/cnr/isti/pad/SearchWord.java
+++ exercises/4. searchword/src/main/java/it/cnr/isti/pad/SearchWord.java
@@ -1,4 +1,4 @@
-package it.cnr.isti.pa;
+package it.cnr.isti.pad;
 
 import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
@@ -16,23 +16,34 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-public class Torture extends Configured implements Tool {
-   public static void main(String[] args) throws Exception {
-      int res = ToolRunner.run(new Configuration(), new Torture(), args);
-      
+public class SearchWord extends Configured implements Tool
+{
+   public static void main(String[] args) throws Exception
+   {
+      int res = ToolRunner.run(new Configuration(), new SearchWord(), args);
+
       System.exit(res);
    }
 
    @Override
-   public int run(String[] args) throws Exception {
-      Job job = Job.getInstance(getConf());
-      
-      job.setJarByClass(Torture.class);
+   public int run(String[] args) throws Exception
+   {
+      if (args.length != 3) {
+          System.out.println("Usage: ./SearchWord inputPath outputPath wordToSearch");
+          System.exit(1);
+      }
+
+      Configuration conf = new Configuration();
+      conf.set("wordToSearch", args[2]);
+
+      Job job = new Job(conf, "searchWord");
+      job.setJarByClass(SearchWord.class);
+
       job.setOutputKeyClass(Text.class);
       job.setOutputValueClass(NullWritable.class);
 
       job.setMapperClass(Map.class);
-      
+
       // Map-only job, since the reducer has nothing to do.
       job.setNumReduceTasks(0);
 
@@ -44,15 +55,22 @@ public class Torture extends Configured implements Tool {
 
       return job.waitForCompletion(true) ? 0 : 1;
    }
-   
-   public static class Map extends Mapper<LongWritable, Text, Text, NullWritable> {
-      /**
-       * The map() method emits lines that contain the word &quote;torture&quote;.
-       */
+
+   public static class Map extends Mapper<LongWritable, Text, Text, NullWritable>
+   {
+      private String wordToSearch;
+
       @Override
-      public void map(LongWritable key, Text value, Context context)
-              throws IOException, InterruptedException {
-         if (value.toString().matches(".*\\b[Tt]orture\\b.*")) {
+      public void setup(Context context)
+      {
+         Configuration conf = context.getConfiguration();
+         this.wordToSearch = conf.get("wordToSearch");
+      }
+
+      @Override
+      public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
+      {
+         if (value.toString().contains(wordToSearch)) {
             context.write(value, NullWritable.get());
          }
       }
